6f0b023435c8287a4270706ffa51e1bf4b465e2d
Sam A. Horvath-Hunt <samhh.web@gmail.com>
2015-11-24 12:46:08 +0000

    Added Piwik analytics

--- content_scripts.php
+++ content_scripts.php
@@ -1,3 +1,18 @@
 <script src="node_modules/lodash/index.js"></script>
 
 <script src="js/main.js"></script>
+
+<!-- Piwik -->
+<script type="text/javascript">
+  var _paq = _paq || [];
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//samhh.com/analytics/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', 1]);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<noscript><p><img src="//samhh.com/analytics/piwik.php?idsite=1" style="border:0"></p></noscript>
