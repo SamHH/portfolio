a59e7bac73febc80467fcab08b6b024f4a2701b4
Sam A. Horvath-Hunt <samhh.web@gmail.com>
2016-04-12 18:41:35 +0100

    Initial commit towards new layout

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,22 +1,6 @@
 module.exports = function (req, res) {
-  const jsonfile = require('jsonfile')
-  const jsonPath = './app/data/home.json'
-
-  jsonfile.readFile(jsonPath, function (err, obj) {
-    if (err) {
-      console.log(err)
-      res.redirect('/error')
-      // Else as otherwise script continues before json file has been read
-    } else renderPage(obj.home)
+  res.render('pages/home', {
+    devenv: process.env.DEV_ENV,
+    title: 'Home'
   })
-
-  function renderPage (data) {
-    res.render('pages/home', {
-      devenv: process.env.DEV_ENV,
-      title: 'Home',
-      expertise: data.expertise,
-      projects: data.projects,
-      alternateColors: data.alternateColors
-    })
-  }
 }
